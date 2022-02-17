518040d3c6ef693ab259416788b2d78bc496e26a
bhaveshajani <bhaveshajani177@gmail.com>
2022-02-17 14:30:01 +0530

    add examples on variables declaration

---
+++ TypeSscript Code Practice/Variables/variables.ts
@@ -0,0 +1,23 @@
+// number
+const pi: number = 3.14;
+
+/*
+    const age: number = "23";
+    // give error: Type 'string' is not assignable to type 'number'.
+*/
+
+// string
+const myName: string = "Bhavesh Ajani";
+
+/*
+    const myName: string = 23;
+    // give error: Type 'number' is not assignable to type 'string'.
+*/
+
+// union types
+const answerA: string | number = "Yes";
+const answerB: string | number = 7;
+
+/**
+    You can define ither string value or number value using union type
+*/
