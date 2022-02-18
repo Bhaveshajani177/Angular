d71ce8c5a2dff4031cf59240ef9a78dd6ca28976
bhaveshajani <bhaveshajani177@gmail.com>
2022-02-18 14:29:07 +0530

    add notes on  ways to convert string into number

---
+++ Ways To Convert String Into Number/script.js
@@ -0,0 +1,32 @@
+var numA = parseInt("1");
+console.log(numA);
+
+var numB = ~~"1";
+console.log(numB);
+
+var numC = "1" - 0;
+console.log(numC);
+
+var numD = "1" * 0;
+console.log(numD);
+
+var numE = +"1";
+console.log(numE);
+
+var numF = Math.floor("1");
+console.log(numF);
+
+var numG = Math.round("1");
+console.log(numG);
+
+var numH = Number("1");
+console.log(numH);
+
+var numI = parseFloat("1");
+console.log(numI);
+
+var numJ = "1" / 1;
+console.log(numJ);
+
+var numk = -"1";
+console.log(numk);
