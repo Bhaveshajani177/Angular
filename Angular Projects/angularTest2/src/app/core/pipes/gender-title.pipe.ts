5ad8e88d8efa4350be94b650b31df9e178016e01
bhaveshajani <bhaveshajani177@gmail.com>
2022-03-01 18:56:54 +0530

    update pipe validation

--- Angular Projects/angularTest2/src/app/core/pipes/gender-title.pipe.ts
+++ Angular Projects/angularTest2/src/app/core/pipes/gender-title.pipe.ts
@@ -7,7 +7,9 @@ export class GenderTitlePipe implements PipeTransform {
   // If Gender is Male prepend Mr.
   // If Gender is Female prepend Ms.
   transform(value: string, ...args: string[]): any {
-    if (value.toLowerCase() === 'male') {
+    if (value === null || value === undefined) {
+      return '-';
+    } else if (value.toLowerCase() === 'male') {
       return 'Mr.';
     } else if (value.toLowerCase() === 'female') {
       return 'Ms.';
